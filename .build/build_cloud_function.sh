71a1da53bc7a57cbdf7fcac2a6c705af661d866f
xxyz30 <ts187579@163.com>
2024-10-02 01:08:24 +0800

    云函数编译脚本

--- .build/build_cloud_function.sh
+++ .build/build_cloud_function.sh
@@ -1,6 +1,5 @@
 # 构建云函数,必须使用linux编译
 cd ../
 pyinstaller --distpath ./dist/cloud_functions -F index.py
-mkdir dist/cloud_functions
 touch dist/cloud_functions/INPUT_HYPERGRYPH_TOKEN.txt
-echo "./index" > bootstrap
+echo "./index" > dist/cloud_functions/bootstrap
